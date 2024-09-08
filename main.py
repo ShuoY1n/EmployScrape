204a95aedc2bc4fc23c8b888d8da9494dd1defe5
ShuoY1n <jackshuoyi@gmail.com>
2024-09-08 11:49:28 -0600

    implemented scraper to scrape the headline and its information

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+from os import major
 import httpx
 from bs4 import BeautifulSoup
 
-url = 'https://www.cnn.com/'
+url = 'https://www.bbc.com/news'
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
 }
@@ -11,7 +12,23 @@ def main():
     response.html = response.text
 
     soup = BeautifulSoup(response.html, 'html.parser')
-    print(soup.find_all('a'))
+
+    majorHeadline = soup.find('div', class_='sc-666b6d83-0 hWNlku')
+
+    majorHeadlineText = majorHeadline.find(attrs={'data-testid': 'card-headline'}).text
+    majorHeadlineDescription = majorHeadline.find(attrs={'data-testid': 'card-description'}).text
+    majorHeadlineElapsedTime = majorHeadline.find(attrs={'data-testid': 'card-metadata-lastupdated'}).text
+    majorHeadlineLocation = majorHeadline.find(attrs={'data-testid': 'card-metadata-tag'}).text
+
+
+    majorHeadlineLink = 'https://www.bbc.com' + majorHeadline.find(attrs={'data-testid': 'internal-link'}).get('href')
+
+    print(majorHeadlineText)
+    print(majorHeadlineDescription)
+    print(majorHeadlineElapsedTime)
+    print(majorHeadlineLocation)
+    print(majorHeadlineLink)
+
 
 
 if __name__ == '__main__':

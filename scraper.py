b16c9b7c17447f3d626eed2f4de87808cfa538e2
ShuoY1n <jackshuoyi@gmail.com>
2024-09-10 19:29:12 -0600

    implemented scraper to scrape all headlines and information

---
+++ scraper.py
@@ -0,0 +1,41 @@
+from os import major
+import httpx
+from bs4 import BeautifulSoup
+
+url = 'https://www.bbc.com/news'
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
+}
+
+cardClass = 'sc-225578b-0 btdqbl'
+
+def main():
+    response = httpx.get(url, headers=headers, follow_redirects=True)
+    response.html = response.text
+
+    soup = BeautifulSoup(response.html, 'html.parser')
+
+    headlines = soup.find_all('div', class_=cardClass)
+
+    for headline in headlines:
+        try:
+            headlineText = headline.find(attrs={'data-testid': 'card-headline'}).text
+            headlineDescription = headline.find(attrs={'data-testid': 'card-description'}).text
+            headlineElapsedTime = headline.find(attrs={'data-testid': 'card-metadata-lastupdated'}).text
+            headlineLocation = headline.find(attrs={'data-testid': 'card-metadata-tag'}).text
+            headlineLink = 'https://www.bbc.com' + headline.find(attrs={'data-testid': 'internal-link'}).get('href')
+        except:
+            continue
+
+        print(headlineText)
+        print(headlineDescription)
+        print(headlineElapsedTime)
+        print(headlineLocation)
+        print(headlineLink)
+        print('--------------------------------')
+
+
+
+
+if __name__ == '__main__':
+    main()

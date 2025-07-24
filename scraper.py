21dd9be9544c766d9f60076fce301727b64dfd7c
ShuoY1n <jackshuoyi@gmail.com>
2025-07-23 18:06:36 -0600

    added gemeni

--- scraper.py
+++ scraper.py
@@ -2,21 +2,25 @@ import httpx
 from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os
+from google import genai
+from google.genai import types
 
 load_dotenv()
 
 notion_key = os.getenv('NOTION_API_KEY')
-gemini_key = os.getenv('GEMINI_API_KEY')
-
-url = 'https://www.bbc.com/news'
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
-}
-
-cardClass = 'sc-225578b-0 btdqbl'
-headlines = []
+notion_database_id = os.getenv('NOTION_DATABASE_ID')
 
 def main():
+    client = genai.Client()
+    
+    cardClass = 'sc-225578b-0 btdqbl'
+    headlines = []
+
+    url = 'https://www.bbc.com/news'
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36'
+    }
+    
     response = httpx.get(url, headers=headers, follow_redirects=True)
     response.html = response.text
 
@@ -24,10 +28,16 @@ def main():
 
     headlines = soup.find_all('div', class_=cardClass)
 
+    count = 0
+
     for headline in headlines:
+        count += 1
+        if count > 5:
+            break
+
         try:
             headlineText = headline.find(attrs={'data-testid': 'card-headline'}).text
-            headlineDescription = headline.find(attrs={'data-testid': 'card-description'}).text
+            headlineBlurb = headline.find(attrs={'data-testid': 'card-description'}).text
             headlineElapsedTime = headline.find(attrs={'data-testid': 'card-metadata-lastupdated'}).text
             headlineCategory = headline.find(attrs={'data-testid': 'card-metadata-tag'}).text
             headlineLink = 'https://www.bbc.com' + headline.find(attrs={'data-testid': 'internal-link'}).get('href')
@@ -38,13 +48,30 @@ def main():
             headlines.append(headlineText)
         else:
             continue
+
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            config=types.GenerateContentConfig(
+                system_instruction=(
+                    "You are a professional summarizer that condenses news into short, accurate briefs. Always include all key facts and avoid injecting opinions."
+                )
+            ),
+            contents=(
+                f"Summarize the following news article in no more than 4 sentences. Include all critical information such as who, what, when, where, why, and how, while preserving factual accuracy. Avoid unnecessary details or commentary. Write in a neutral tone. ARTICLE: {headlineText}"
+            )
+        )
         
-        print(headlineText)
-        print(headlineDescription)
-        print(headlineElapsedTime)
-        print(headlineCategory)
-        print(headlineLink)
+        print(headlineText + '\n--------------------------------')
+        print(response.text)
         print('--------------------------------')
+
+
+        # print(headlineText)
+        # print(headlineBlurb)
+        # print(headlineElapsedTime)
+        # print(headlineCategory)
+        # print(headlineLink)
+        # print('--------------------------------')
 
 
 
